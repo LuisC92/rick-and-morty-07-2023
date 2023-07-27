651a235c07893ad4e2c6bcd5754718959e6587b3
LuisC92 <luis.clara@wildcodeschool.com>
2023-07-27 17:17:28 +0100

    create CharactersCard component and pass data as props to render the info

--- src/components/CharactersCard/CharactersCard.jsx
+++ src/components/CharactersCard/CharactersCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from "react";
 
-const CharactersCard = () => {
+const CharactersCard = ({ characters }) => {
   return (
-    <div>CharactersCard</div>
-  )
-}
+    <div>
+      {characters.map((character) => (
+        <div key={character.id}>
+          <img src={character.image} alt={character.name} />
+          <h1>{character.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default CharactersCard
+export default CharactersCard;
--- src/components/CharactersList/CharactersList.jsx
+++ src/components/CharactersList/CharactersList.jsx
@@ -1,18 +1,22 @@
+import {useState} from "react"
 import CharactersCard from "../CharactersCard/CharactersCard";
 import axios from "axios"
 // https://rickandmortyapi.com/api/character
 const CharactersList = () => {
 
+    const [characters, setCharacters] = useState([])
+
     const getCharacters = () =>{
         axios.get("https://rickandmortyapi.com/api/character")
-            .then(response => console.log(response.data.results))
+            .then(response => setCharacters(response.data.results))
     }
 
+    console.log(characters)
   return (
     <div>
       CharactersList
       <button onClick={getCharacters}>Call API</button>
-      <CharactersCard />
+      <CharactersCard characters={characters} />
     </div>
   );
 };
